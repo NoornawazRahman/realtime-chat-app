411fc8eeeeada3411eeab4b9538376d4861324ee
Md Noornawaz Rahman <noornawazrahman@gmail.com>
2026-01-03 06:34:34 +0530

    feat: implement welcome email

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,28 +1,29 @@
+import { sendWelcomeEmail } from "../emails/emailHandlers.js";
+import { generateToken } from "../lib/utils.js";
 import User from "../models/User.js";
 import bcrypt from "bcryptjs";
-import { generateToken } from "../lib/utils.js";
+import { ENV } from "../lib/env.js";
 
 export const signup = async (req, res) => {
     const { fullName, email, password } = req.body;
+
     try {
         if (!fullName || !email || !password) {
             return res.status(400).json({ message: "All fields are required" });
         }
+
         if (password.length < 6) {
-            return res
-                .status(400)
-                .json({ message: "password must be at least 6 characters" });
+            return res.status(400).json({ message: "Password must be at least 6 characters" });
         }
 
-        // check if email is valid: regex
+        // check if emailis valid: regex
         const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
         if (!emailRegex.test(email)) {
             return res.status(400).json({ message: "Invalid email format" });
         }
+
         const user = await User.findOne({ email });
-        if (user) {
-            return res.status(400).json({ message: "Email already exists" });
-        }
+        if (user) return res.status(400).json({ message: "Email already exists" });
 
         // 123456 => $dnjasdkasj_?dmsakmk
         const salt = await bcrypt.genSalt(10);
@@ -50,10 +51,17 @@ export const signup = async (req, res) => {
                 email: newUser.email,
                 profilePic: newUser.profilePic,
             });
+
+            try {
+                await sendWelcomeEmail(savedUser.email, savedUser.fullName, ENV.CLIENT_URL);
+            } catch (error) {
+                console.error("Failed to send welcome email:", error);
+            }
+        } else {
+            res.status(400).json({ message: "Invalid user data" });
         }
     } catch (error) {
         console.log("Error in signup controller:", error);
         res.status(500).json({ message: "Internal server error" });
     }
 };
-// 1:07:00
